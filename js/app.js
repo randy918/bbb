53ab9f71da214196fac49c306788c38bd483080f
randy918 <randy918@gmail.com>
2022-01-24 19:24:49 -0600

    removed attempt,  kep menu disappearing at 600 max width

--- js/app.js
+++ js/app.js
@@ -3,14 +3,6 @@ window.onload = function () {
 };
 
 function init() {
-  const toggleButton = document.getElementsByClassName("toggle-button")[0];
-
-  const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-
-  toggleButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle("active");
-  });
-
   $(".headline").click(function () {
     window.location = $(this).find("a").attr("href");
     return false;
